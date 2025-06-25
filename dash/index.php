9233dc4e9aaf8f4b2729951dc8a6b5527f4a2b75
HermaVPN <141176675+hermavpn@users.noreply.github.com>
2025-06-25 15:39:48 +0330

    Update index.php

--- dash/index.php
+++ dash/index.php
@@ -17,7 +17,7 @@ $ch = curl_init();
 curl_setopt_array($ch, [
     CURLOPT_URL => $url,
     CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_SSL_VERIFYPEER => false,
+    CURLOPT_SSL_VERIFYPEER => true,
     CURLOPT_SSL_VERIFYHOST => 2,
     CURLOPT_HEADERFUNCTION => function($curl, $header) {
         header($header);
